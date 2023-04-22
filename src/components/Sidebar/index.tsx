42344b2300a6c0a6f9351e8e20d95ae7ceadb29e
theluanhoang <hoangtheluan2016@gmail.com>
2023-04-22 15:16:43 +0700

    feat: remove function create blog

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,7 +3,6 @@ import { FaHome, FaUserAlt, FaShoppingBag } from 'react-icons/fa'
 import { HiMailOpen } from 'react-icons/hi'
 import { GiAchievement } from 'react-icons/gi'
 import { Link } from 'react-router-dom'
-import { HiPencilAlt } from 'react-icons/hi'
 import ESidebar from '../../interfaces/ESidebar'
 
 function Sidebar() {
@@ -11,11 +10,6 @@ function Sidebar() {
 
     return (
         <>
-            <li className={`fixed z-50 top-[30px] right-[30px] sidebar-item ${currentPage === ESidebar.CREATEBLOG ? 'sidebar-item--active' : ''} `} onClick={() => setCurrentPage(ESidebar.CREATEBLOG)}>
-                <Link to='/create-blog' className={`sidebar-item ${currentPage === ESidebar.CREATEBLOG ? 'sidebar-item--active' : ''}`}>
-                    <HiPencilAlt size={20} color='#fff' />
-                </Link>
-            </li>
             <div className='fixed z-50 mymd:top-[50%] mymd:-translate-y-[50%] mymd:right-[30px] mymd:translate-x-[0] bottom-[20px] right-[50%] translate-x-[50%]'>
                 <div className='flex flex-row mymd:flex-col gap-[20px] '>
                     <Link to={ESidebar.HOME} className={`sidebar-item ${currentPage === ESidebar.HOME ? 'sidebar-item--active' : ''} `} onClick={() => setCurrentPage(ESidebar.HOME)}>
