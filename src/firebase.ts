d11a37e6053f609e45ab06bb5c870f5dfae6ccd2
luantrum27 <hoangtheluan2016@gmail.com>
2023-04-22 09:13:01 +0700

    feat: deloy website on firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,24 @@ import { getDatabase } from "firebase/database";
 // import "firebase/firestore";
 // import {config} from 'dotenv'
 // config()
+// const firebaseConfig = {
+//   apiKey: "AIzaSyABgOIaqqCV2gCWprFcVr5RD09y5lvD50o",
+//   authDomain: "portfolio-d9079.firebaseapp.com",
+//   projectId: "portfolio-d9079",
+//   storageBucket: "portfolio-d9079.appspot.com",
+//   messagingSenderId: "565951968085",
+//   appId: "1:565951968085:web:ba50ac9c2ad21e620db5dc",
+//   measurementId: "G-B3G0V1QK6T"
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyABgOIaqqCV2gCWprFcVr5RD09y5lvD50o",
   authDomain: "portfolio-d9079.firebaseapp.com",
+  databaseURL: "https://portfolio-d9079-default-rtdb.firebaseio.com",
   projectId: "portfolio-d9079",
   storageBucket: "portfolio-d9079.appspot.com",
   messagingSenderId: "565951968085",
-  appId: "1:565951968085:web:ba50ac9c2ad21e620db5dc",
-  measurementId: "G-B3G0V1QK6T"
+  appId: "1:565951968085:web:162480a3a5f8fb9e0db5dc",
+  measurementId: "G-DCC46V973D"
 };
 
 // Initialize Firebase
